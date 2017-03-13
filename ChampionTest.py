846078eb0377a7b8a136cf2c5fe88d8d3aef4807
Jarek <jarekcrowder@gmail.com>
2017-03-13 17:37:44 -0500

    Basic Champion Stats

---
+++ ChampionTest.py
@@ -0,0 +1,41 @@
+print ("Hello Python")
+Champions = {'Aatrox': 'Aatrox', 'Ahri': 'Ahri', 'Akali': 'Akali', 'Alistar': 'Alistar', 'Amumu': 'Amumu', 'Anivia': 'Anivia', 'Annie': 'Annie', 'Ashe': 'Ashe', 'Aurelion Sol': 'Aurelion Sol',
+             'Azir': 'Azir', 'Bard': 'Bard', 'Blitzcrank': 'Blitzcrank', 'Brand': 'Brand', 'Braum': 'Braum', 'Caitlyn': 'Caitlyn', 'Camille': 'Camille', 'Cassiopeia': 'Cassiopeia', 'Cho Gath': 'Cho Gath',
+             'Corki': 'Corki', 'Darius': 'Darius', 'Diana': 'Diana', 'Dr. Mundo': 'Dr. Mundo', 'Draven': 'Draven', 'Ekko': 'Ekko', 'Elise': 'Elise', 'Evelynn': 'Evelynn', 'Ezreal': 'Ezreal', 'Fiddlesticks':
+             'Fiddlesticks', 'Fiora': 'Fiora', 'Fizz': 'Fizz', 'Galio': 'Galio', 'Gangplank': 'Gangplank', 'Garen': 'Garen', 'Gnar': 'Gnar', 'Gragas': 'Gragas', 'Graves': 'Graves', 'Hecarim': 'Hecarim',
+             'Heimerdinger': 'Heimerdinger', 'Illaoi': 'Illaoi', 'Irelia': 'Irelia', 'Ivern': 'Ivern', 'Janna': 'Janna', 'Jarvan IV': 'Jarvan IV', 'Jax': 'Jax', 'Jayce': 'Jayce', 'Jhin': 'Jhin', 'Jinx':
+             'Jinx', 'Kalista': 'Kalista', 'Karma': 'Karma', 'Karthus': 'Karthus', 'Kassadin': 'Kassadin', 'Katarina': 'Katarina', 'Kayle': 'Kayle', 'Kennen': 'Kennen', 'Kha Zix': 'Kha Zix', 'Kindred':
+             'Kindred', 'Kled': 'Kled', 'Kog Maw': 'Kog Maw', 'LeBlanc': 'LeBlanc', 'Lee Sin': 'Lee Sin', 'Leona': 'Leona', 'Lissandra': 'Lissandra', 'Lucian': 'Lucian', 'Lulu': 'Lulu', 'Lux': 'Lux',
+             'Malphite': 'Malphite', 'Malzahar': 'Malzahar', 'Maokai': 'Maokai', 'Master Yi': 'Master Yi', 'Miss Fortune': 'Miss Fortune', 'Mordekaiser': 'Mordekaiser', 'Morgana': 'Morgana', 'Nami':
+             'Nami', 'Nasus': 'Nasus', 'Nautilus': 'Nautilus', 'Nidalee': 'Nidalee', 'Nocturne': 'Nocturne', 'Nunu': 'Nunu', 'Olaf': 'Olaf', 'Orianna': 'Orianna', 'Pantheon': 'Pantheon', 'Poppy':
+             'Poppy', 'Quinn': 'Quinn', 'Rammus': 'Rammus', 'Rek Sai': 'Rek Sai', 'Renekton': 'Renekton', 'Rengar': 'Rengar', 'Riven': 'Riven', 'Rumble': 'Rumble', 'Ryze': 'Ryze', 'Sejuani': 'Sejuani',
+             'Shaco': 'Shaco', 'Shen': 'Shen', 'Shyvana': 'Shyvana', 'Singed': 'Singed', 'Sion': 'Sion', 'Sivir': 'Sivir', 'Skarner': 'Skarner', 'Sona': 'Sona', 'Soraka': 'Soraka', 'Swain': 'Swain',
+             'Syndra': 'Syndra', 'Tahm Kench': 'Tahm Kench', 'Taliyah': 'Taliyah', 'Talon': 'Talon', 'Taric': 'Taric', 'Teemo': 'Teemo', 'Thresh': 'Thresh', 'Tristana': 'Tristana', 'Trundle': 'Trundle',
+             'Tryndamere': 'Tryndamere', 'Twisted Fate': 'Twisted Fate', 'Twitch': 'Twitch', 'Udyr': 'Udyr', 'Urgot': 'Urgot', 'Varus': 'Varus', 'Vayne': 'Vayne', 'Veigar': 'Veigar', 'Vel Koz': 'Vel Koz',
+             'Vi': 'Vi', 'Viktor': 'Viktor', 'Vladimir': 'Vladimir', 'Volibear': 'Volibear', 'Warwick': 'Warwick', 'Wukong': 'Wukong', 'Xerath': 'Xerath', 'Xin Zhao': 'Xin Zhao', 'Yasuo': 'Yasuo', 'Yorick':
+             'Yorick', 'Zac': 'Zac', 'Zed': 'Zed', 'Ziggs': 'Ziggs', 'Zilean': 'Zilean', 'Zyra': 'Zyra' 
+             }
+print (Champions['Aatrox'] + " " + Champions['Malzahar'] + " " + Champions['Ziggs'])
+
+ChampionsHealth = {'Aatrox': '580', 'Ahri': '514.4', 'Akali': '587.8', 'Alistar': '613.36', 'Amumu': '613.12', 'Anivia': '467.6', 'Annie': '511.68', 'Ashe': '527.72', 'Aurelion Sol': '550',
+             'Azir': '524.4', 'Bard': '535', 'Blitzcrank': '582.6', 'Brand': '507.68', 'Braum': '576.16', 'Caitlyn': '524.4', 'Camille': '575.6', 'Cassiopeia': '525', 'Cho Gath': '574.4',
+             'Corki': '512.76', 'Darius': '582.24', 'Diana': '589.2', 'Dr. Mundo': '582.52', 'Draven': '557.76', 'Ekko': '580', 'Elise': '529.4', 'Evelynn': '531.2', 'Ezreal': '484.4', 'Fiddlesticks':
+             '524.4', 'Fiora': '550', 'Fizz': '558.48', 'Galio': '577.8', 'Gangplank': '540', 'Garen': '616.28', 'Gnar': '540', 'Gragas': '583.52', 'Graves': '551.12', 'Hecarim': '580.6',
+             'Heimerdinger': '476', 'Illaoi': '585.6', 'Irelia': '607.2', 'Ivern': '580', 'Janna': '487.04', 'Jarvan IV': '571.2', 'Jax': '592.8', 'Jayce': '571.2', 'Jhin': '540', 'Jinx':
+             '517.76', 'Kalista': '517.76', 'Karma': '522.44', 'Karthus': '516', 'Kassadin': '564.04', 'Katarina': '590', 'Kayle': '574.24', 'Kennen': '535.72', 'Kha Zix': '572.8', 'Kindred':
+             '540', 'Kled': '340', 'Kog Maw': '517.76', 'LeBlanc': '516', 'Lee Sin': '570.8', 'Leona': '576.16', 'Lissandra': '506.12', 'Lucian': '554.4', 'Lulu': '552.76', 'Lux': '477.72',
+             'Malphite': '574.2', 'Malzahar': '514.4', 'Maokai': '572.2', 'Master Yi': '598.56', 'Miss Fortune': '530', 'Mordekaiser': '525', 'Morgana': '547.48', 'Nami':
+             '489.32', 'Nasus': '561.2', 'Nautilus': '576.48', 'Nidalee': '540', 'Nocturne': '582.8', 'Nunu': '598.28', 'Olaf': '597.24', 'Orianna': '517.72', 'Pantheon': '579.16', 'Poppy':
+             '540', 'Quinn': '532.8', 'Rammus': '564.48', 'Rek Sai': '570', 'Renekton': '572.16', 'Rengar': '586.2', 'Riven': '558.48', 'Rumble': '584.4', 'Ryze': '558.48', 'Sejuani': '600',
+             'Shaco': '582.12', 'Shen': '540', 'Shyvana': '594.6', 'Singed': '542.76', 'Sion': '542.64', 'Sivir': '515.76', 'Skarner': '601.28', 'Sona': '482.36', 'Soraka': '529.04', 'Swain': '516.04',
+             'Syndra': '511.04', 'Tahm Kench': '610', 'Taliyah': '520', 'Talon': '582.8', 'Taric': '575', 'Teemo': '515.76', 'Thresh': '560.52', 'Tristana': '542.76', 'Trundle': '616.28',
+             'Tryndamere': '625.64', 'Twisted Fate': '521.76', 'Twitch': '525.08', 'Udyr': '593.32', 'Urgot': '586.52', 'Varus': '537.76', 'Vayne': '498.44', 'Veigar': '492.76', 'Vel Koz': '507.68',
+             'Vi': '582.8', 'Viktor': '516.01', 'Vladimir': '525', 'Volibear': '584.48', 'Warwick': '550', 'Wukong': '577.8', 'Xerath': '514.4', 'Xin Zhao': '600', 'Yasuo': '517.76', 'Yorick':
+             '580', 'Zac': '614.6', 'Zed': '579.4', 'Ziggs': '524.4', 'Zilean': '499.28', 'Zyra': '499' 
+             }
+
+
+
+
+
+"'': ''," 
